1f8faf10a778276a58f34ed51017c7a124ec76b3
David Bindel <bindel@cs.cornell.edu>
2017-09-06 14:27:22 -0400

    Updated fetch-repo to forgive https URLs for repos.

--- util/fetch-repo.sh
+++ util/fetch-repo.sh
@@ -7,4 +7,9 @@ else
 fi
 
 REPO=$(head -n 1 $RFILE)
+if echo $REPO | grep "^https" ; then
+  echo "Looks like an HTTPS URL; convert"
+  REPO=`echo $REPO | sed 's/https:[/][/]/git@/' | sed 's/[/]/:/'`
+fi
+echo "Fetch from $REPO"
 ssh-agent bash -c "ssh-add $RFILE ; git clone $REPO"
